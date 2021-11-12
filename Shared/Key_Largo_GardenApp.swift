b0dc84b5cef4ac48f3529f13819d765bc4601349
tjrunnels <tom@forwardtechfl.com>
2021-11-12 12:16:34 -0500

    Initial Commit

---
+++ Shared/Key_Largo_GardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Key_Largo_GardenApp.swift
+//  Shared
+//
+//  Created by Tom Runnels on 11/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Key_Largo_GardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
